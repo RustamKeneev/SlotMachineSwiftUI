d73d023d5458a51ff80e2af4139e0896ee328520
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-03-21 13:15:32 +0600

    Initial Commit

---
+++ SlotMachineSwiftUI/SlotMachineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineSwiftUIApp.swift
+//  SlotMachineSwiftUI
+//
+//  Created by Rustam Keneev on 21/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
