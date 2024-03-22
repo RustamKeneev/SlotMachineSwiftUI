37172a9047de534b1e56d3ca09b0ccb46e78dfe3
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-03-23 01:13:37 +0600

    added game over and  setting bussines logis

--- SlotMachineSwiftUI/Views/ContentView.swift
+++ SlotMachineSwiftUI/Views/ContentView.swift
@@ -17,13 +17,11 @@ struct ContentView: View {
     @State private var betAmmount: Int = 10
     @State private var isActiveBet10: Bool = true
     @State private var isActiveBet20: Bool = false
+    @State private var showingModal: Bool = false
     
     //MARK: - FUNCTIONS
     //SPIN REELS
     func spinReels(){
-//        reels[0] = Int.random(in: 0...symbols.count - 1)
-//        reels[1] = Int.random(in: 0...symbols.count - 1)
-//        reels[2] = Int.random(in: 0...symbols.count - 1)
         reels = reels.map({ _ in
             Int.random(in: 0...symbols.count - 1)
         })
@@ -70,6 +68,12 @@ struct ContentView: View {
     }
     
     //GAME IS OVER
+    func isGameOver(){
+        if coins <= 0 {
+            //SHOW MODAL WINDOW
+            showingModal = true
+        }
+    }
     
 
     var body: some View {
@@ -147,6 +151,9 @@ struct ContentView: View {
                         
                         //CHECK WINNING
                         self.checkWinning()
+                        
+                        //GAME IS OVER
+                        self.isGameOver()
                     }, label: {
                         Image("gfx-spin")
                             .renderingMode(.original)
@@ -225,7 +232,65 @@ struct ContentView: View {
 
             .padding()
 //            .frame(minWidth: 720)
+            .blur(radius: $showingModal.wrappedValue ? 5 : 0, opaque: false)
             //MARK: - POPUP
+            if $showingModal.wrappedValue {
+                ZStack{
+                    Color("ColorTransparentBlack")
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    //MODAL
+                    VStack(spacing: 0){
+                        //TITLE
+                        Text("GAME OVER")
+                            .font(.system(.title, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding()
+                            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+                            .background(Color("ColorPink"))
+                            .foregroundColor(Color.white)
+                        Spacer()
+                        
+                        //MESSAGE
+                        VStack(alignment: .center, spacing: 16){
+                            Image("gfx-seven-reel")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 72)
+                            
+                            Text("Bad luck! You lost all of the coins. \nLet's play again!")
+                                .font(.system(.body, design: .rounded))
+                                .lineLimit(2)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color.gray)
+                                .layoutPriority(1)
+                            
+                            Button(action: {
+                                self.showingModal = false
+                                self.coins = 100
+                            }){
+                                Text("New Game".uppercased())
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.semibold)
+                                    .accentColor(Color("ColorPink"))
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 8)
+                                    .frame(minWidth: 128)
+                                    .background(
+                                        Capsule()
+                                            .strokeBorder(lineWidth: 1.76 )
+                                            .foregroundColor(Color("ColorPink"))
+                                    )
+                            }
+                        }
+                        Spacer()
+                    }
+                    .frame(minWidth: 280,idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight: 320, alignment: .center)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+                }
+            }
         }//: ZSTACK
         .sheet(isPresented: $showingInfoView){
             InfoView()
